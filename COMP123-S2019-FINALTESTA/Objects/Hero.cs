9964f615e811888e5b7ab6d228c88faa733d5838
301044427 <301044427@PR-E216-13>
2019-08-09 09:06:15 -0400

    Base Project

---
+++ COMP123-S2019-FINALTESTA/Objects/Hero.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 
+    Student name = Aniket passi
+    studentID = 301044427
+    Description : this is the hero data container class
+     */
+namespace COMP123_S2019_FINALTESTA.Objects
+{
+    class Hero
+    {
+      
+
+        public string HeroName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //physical abilities
+        public string Fighting { get; set; }
+
+        public string Strength { get; set; }
+        public string Agility { get; set; }
+        public string Endurance { get; set; }
+        
+            //mental abilities
+        public string Reason { get; set; }
+        public string Intuition { get; set; }
+        public string Psyche { get; set; }
+        public string Popularity { get; set; }
+        
+
+        //power list
+        List<Power> Powers;
+
+        //constructor
+        Hero()
+        {
+            //instantiate an empty power list
+            Powers = new List<Power>();
+        }
+
+
+    }
+}
---
+++ COMP123-S2019-FINALTESTA/Objects/Power.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ 
+    Student name = Aniket passi
+    studentID = 301044427
+    Description : this is the power class
+     */
+namespace COMP123_S2019_FINALTESTA.Objects
+{
+
+    class Power
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
